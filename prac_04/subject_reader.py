c21dee6ea4154cd4d715fc41bcff2f0c0c043f04
j-manio <john..manio@my.jcu.edu.au>
2022-10-13 22:27:54 +1000

    Use of basic list operation

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,26 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    display_data(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
+    data = []
+    input_file = open(FILENAME, 'r')
+
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        data.append(parts)
     input_file.close()
+    return data
+
+
+def display_data(data):
+    for subject_data in data:
+        print(f'{subject_data[0]} is taught by {subject_data[1]:13} and has {subject_data[2]:3} students')
 
 
 main()
