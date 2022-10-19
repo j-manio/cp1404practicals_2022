9d161b8a1d2eedb7a4b10a010ff513734affa30c
j-manio <john..manio@my.jcu.edu.au>
2022-10-19 22:11:02 +1000

    Store states abbreviations and names

---
+++ prac_05/state_names.py
@@ -0,0 +1,22 @@
+"""
+CP1404/CP5632 Practical
+State names in a dictionary
+File needs reformatting
+"""
+
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+print(CODE_TO_NAME)
+
+state_code = input("Enter short state: ").upper()
+while state_code != "":
+    if state_code in CODE_TO_NAME:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    else:
+        print("Invalid short state")
+
+    state_code = input("Enter short state: ").upper()
+
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:3} is {name}")
+
