aae1bbe2586d15d3f17efc4bd4f0817d27d6e989
j-manio <john..manio@my.jcu.edu.au>
2022-10-27 16:15:47 +1000

    Created Car object called "limo"

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,6 +9,12 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
+
+    limo = Car(100)
+    limo.add_fuel(20)
+    drove = limo.drive(115)
+
+    print(f"Limo has fuel: {limo.fuel} and it drove {drove}km")
     my_car = Car(180)
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
