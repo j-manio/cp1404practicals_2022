cef4ccc538c7d9cd80808d2afc159b3100c331ad
j-manio <john..manio@my.jcu.edu.au>
2022-11-02 22:18:09 +1000

    str method is working properly

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -10,15 +10,15 @@ from prac_06.car import Car
 def main():
     """Demo test code to show how to use car class."""
 
-    limo = Car(100)
+    limo = Car("Limo", 100)
     limo.add_fuel(20)
-    drove = limo.drive(115)
+    limo.drive(115)
+    print(limo)
 
-    print(f"Limo has fuel: {limo.fuel} and it drove {drove}km")
-    my_car = Car(180)
-    my_car.drive(30)
-    print(f"Car has fuel: {my_car.fuel}")
-    print(my_car)
+    # my_car = Car(180)
+    # my_car.drive(30)
+    # print(f"Car has fuel: {my_car.fuel}")
+    # print(my_car)
 
 
 main()
