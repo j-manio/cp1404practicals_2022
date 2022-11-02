cc9ff1fcb423c4b2589c250070ff7dbfef5de106
j-manio <john..manio@my.jcu.edu.au>
2022-11-02 22:37:32 +1000

    Added and initialised values

---
+++ prac_06/programming_language.py
@@ -0,0 +1,6 @@
+class ProgrammingLanguage:
+    def __init__(self, name="", is_dynamic=False, is_reflection=False, year=0):
+        self.name = name
+        self.is_dynamic = is_dynamic
+        self.reflection = is_reflection
+        self.year = year
