f41ecc794db349d58055501a98af4e4d91135567
j-manio <john..manio@my.jcu.edu.au>
2022-11-03 22:43:02 +1000

    Added a for loop to display the guitar collection

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -4,13 +4,20 @@ from prac_06.guitar import Guitar
 def main():
     guitars = []
 
-    print("My guitar")
+    print("My guitar!")
     guitar_name = input("Name: ")
     while guitar_name != "":
-        manufactured_date = int(input("Date: "))
-
-        print("These are my guitars")
+        manufactured_year = int(input("Year: "))
+        guitar_cost = float(input("Cost: $"))
+        add_guitar = Guitar(guitar_name, manufactured_year, guitar_cost)
+        guitars.append(add_guitar)
+        print(f"{guitar_name} ({manufactured_year}) : ${guitar_cost:.2f} added.")
         guitar_name = input("Name: ")
+
+    for i, guitar in enumerate(guitars, 1):
+        guitar_age = guitar.get_age()
+
+        print(f"Guitar {i}: {guitar.name} ({guitar.year}), worth $ {guitar.cost} ({guitar_age})")
 
 
 main()
