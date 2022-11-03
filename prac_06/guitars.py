c234e0c2f10f89b94fdec172018b42d380e283d2
j-manio <john..manio@my.jcu.edu.au>
2022-11-03 22:16:50 +1000

    Added a name error checking

---
+++ prac_06/guitars.py
@@ -0,0 +1,16 @@
+from prac_06.guitar import Guitar
+
+
+def main():
+    guitars = []
+
+    print("My guitar")
+    guitar_name = input("Name: ")
+    while guitar_name != "":
+        manufactured_date = int(input("Date: "))
+
+        print("These are my guitars")
+        guitar_name = input("Name: ")
+
+
+main()
