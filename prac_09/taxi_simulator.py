a447f32db8c550c32284643e1d232f966cbed4d7
j-manio <john..manio@my.jcu.edu.au>
2022-11-23 23:47:21 +1000

    Imported 3 classes and made a menu

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,13 @@
+"""
+Taxi Simulator
+CP1404 - Practical 9
+"""
+
+from prac_09.car import Car
+from prac_09.taxi import Taxi
+from prac_09.silver_sevice_taxi import SilverServiceTaxi
+
+
+MENU = """ q)uit, c)hoose, d)rive
+"""
+print(MENU)
