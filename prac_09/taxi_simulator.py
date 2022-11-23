655bb69fd03ea344fb8eb45dabe63168586706f8
j-manio <john..manio@my.jcu.edu.au>
2022-11-23 23:53:07 +1000

    Made a menu with error checking

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -8,6 +8,34 @@ from prac_09.taxi import Taxi
 from prac_09.silver_sevice_taxi import SilverServiceTaxi
 
 
-MENU = """ q)uit, c)hoose, d)rive
+MENU = """q)uit, c)hoose, d)rive
 """
-print(MENU)
+
+
+def main():
+    total_bill = 0
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4,)]
+
+    print("Let's drive!")
+    print(MENU)
+    menu_choice = input(">>> ").lower()
+    while menu_choice != "q":
+        if menu_choice == "c":
+            pass
+        elif menu_choice == "d":
+            pass
+        else:
+            print("Invalid option")
+        print("Bill to date: ${total_bill:.2f}")
+        print(MENU)
+        menu_choice = input(">>> ").lower()
+
+
+
+
+
+
+
+
+main()
+
