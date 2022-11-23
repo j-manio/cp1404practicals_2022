23bf1fa46f301cf39c91b58e135f8b0339803045
j-manio <john..manio@my.jcu.edu.au>
2022-11-23 22:50:46 +1000

    Set a certain price per km

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -7,6 +7,7 @@ from prac_09.car import Car
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
+    price_per_km = 1.23
 
     def __init__(self, name, fuel, price_per_km):
         """Initialise a Taxi instance, based on parent class Car."""
