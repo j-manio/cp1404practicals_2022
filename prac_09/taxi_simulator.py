88c0768ca5b2b7235f2f0bf7dae73e4ab275b706
j-manio <john..manio@my.jcu.edu.au>
2022-11-23 23:55:48 +1000

    Added drive code when they choose Drive

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -15,7 +15,7 @@ MENU = """q)uit, c)hoose, d)rive
 def main():
     total_bill = 0
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4,)]
-
+    current_taxi = ""
     print("Let's drive!")
     print(MENU)
     menu_choice = input(">>> ").lower()
@@ -30,7 +30,15 @@ def main():
             except IndexError:
                 print("Invalid taxi choice")
         elif menu_choice == "d":
-            pass
+            if current_taxi:
+                current_taxi.start_fare()
+                distance_to_drive = float(input("Drive how far? "))
+                current_taxi.drive(distance_to_drive)
+                trip_cost = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${trip_cost:.2f}")
+                total_bill += trip_cost
+            else:
+                print("You need to choose a taxi before you can drive")
         else:
             print("Invalid option")
         print("Bill to date: ${total_bill:.2f}")
